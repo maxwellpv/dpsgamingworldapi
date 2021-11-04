51a2f4c662d4e403fb50bfc8c90153e5982fa56c
Aaron <aaron.plan@hotmail.com>
2021-11-04 01:33:23 -0500

    feature: Implements Publications EndPoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Publication } from "./publications/publication.entity";
+import { PublicationsModule } from "./publications/publications.module";
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '123456',
+      database: 'lc2',
+      entities: [Publication],
+      synchronize: true,
+      dropSchema: false,
+    }),
+    PublicationsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
